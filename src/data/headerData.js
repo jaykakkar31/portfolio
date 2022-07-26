ccd0258deef7a3676b9f38e3bb54758315ac2692
jaykakkar31 <jaykakkar31@gmail.com>
2022-07-27 01:08:41 +0530

    resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -7,5 +7,5 @@ export const headerData = {
 		"A passionate Full Stack Web Developer and Mobile App Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks and Cross Platform Mobile Apps With React Native.",
 	image: "img/team22.jpeg",
 	resumePdf:
-		"https://drive.google.com/file/d/1_3CH1kCiklf1Taf38NbS1PD-ict3fi24/view?usp=sharing",
+		"https://drive.google.com/file/d/1rXFf_zhWF4oqNWmV2YGzMAgxa7qHQ7Ga/view?usp=sharing",
 };
